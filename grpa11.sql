5c9d48e0c53a9f6d5def1002fafa0f1cd5c0ef4f
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-12-01 21:37:26 +0100

    database

--- grpa11.sql
+++ grpa11.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Jeu 01 Décembre 2022 à 18:53
+-- Généré le :  Jeu 01 Décembre 2022 à 20:36
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -17,8 +17,41 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `grpa11php`
+-- Base de données :  `grpa11`
 --
+CREATE DATABASE IF NOT EXISTS `grpa11` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `grpa11`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `home`
+--
+
+CREATE TABLE `home` (
+  `h1` varchar(200) NOT NULL,
+  `h2` varchar(200) NOT NULL,
+  `p1` text NOT NULL,
+  `p2` text NOT NULL,
+  `cardp1` text NOT NULL,
+  `cardp2` text NOT NULL,
+  `card1title` varchar(200) NOT NULL,
+  `card2title` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projects`
+--
+
+CREATE TABLE `projects` (
+  `id` int(100) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `img_parallax` varchar(200) NOT NULL,
+  `h2` varchar(100) NOT NULL,
+  `p1` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -47,6 +80,12 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `admin`) VALUES
 --
 
 --
+-- Index pour la table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
@@ -56,6 +95,11 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour les tables exportées
 --
 
+--
+-- AUTO_INCREMENT pour la table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
