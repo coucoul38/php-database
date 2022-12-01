cfe0cc3109cd2e6cb4409e007362e4fdfff4b4c9
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-12-01 19:31:53 +0100

    change img

--- actions/changeImg.php
+++ actions/changeImg.php
@@ -2,18 +2,17 @@
 require_once "../cfg/config.php";
 require "../components/securityAdmin.php";
 $id = $_POST['id'];
-$img = $_FILES['image']['tmp_name'];
 if(empty($id)){
     echo "Error : ID is empty";
 }
 else{
-    if(empty($img)){
+    if(empty($_FILES['image']['tmp_name'])){
         echo "No img";
     }
     else{
         require "upload_file.php";
         echo "ID: ".$id;
-        $sql = "UPDATE projects SET img_parallax = '$img' WHERE id='$id'";
+        $sql = "UPDATE projects SET img_parallax = '$destination' WHERE id='$id'";
         $pre = $pdo->prepare($sql); 
         $pre->execute();
         echo $img;
