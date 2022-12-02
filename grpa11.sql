ee54d59ca9052c92e599df9802ee11e1db446e3f
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-12-02 01:19:51 +0100

    ayo the pizza here

--- grpa11.sql
+++ grpa11.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Jeu 01 Décembre 2022 à 22:32
+-- Généré le :  Ven 02 Décembre 2022 à 00:01
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -67,8 +67,9 @@ CREATE TABLE `projects` (
 --
 
 INSERT INTO `projects` (`id`, `name`, `img_parallax`, `h2`, `p1`, `description`) VALUES
-(4, 'Test img accueil', 'img/grief.jpg', 'h222', 'Amogus jifejfrjfijeifje', 'Je suis une description'),
-(5, 'Noham', 'img/Noham Coulon.jpg', NULL, NULL, 'Je suis une description');
+(19, 'Once grief enters your life, it never leaves', 'img/grief.jpg', 'Once grief enters your life, it never leaves', 'Once grief enters your life, it never leaves', 'Once grief enters your life, it never leaves'),
+(21, 'Entrez un titre', 'img/Noham Coulon.jpg', 'h2', 'Text', 'Je suis une description'),
+(22, 'Star Wars', 'img/163-1636953_star-wars-wallpaper-1080p-star-wars-battlefront-2.jpg', 'Episode IV A NEW HOPE', 'It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire\'s\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power to\r\ndestroy an entire planet.\r\n\r\nPursued by the Empire\'s\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save\r\nher people and restore\r\nfreedom to the galaxy....', '');
 
 -- --------------------------------------------------------
 
@@ -90,7 +91,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `admin`) VALUES
 (9, 'Coucoul38', 'noham.coulon@gmail.com', '3c6e50083f1846975d8b8fe3517409baef5ed11488935fea1bf0051a5e3ff9c1', 1),
-(10, 'bobby', 'bob.bob@bob.bob', 'd8ba1ff038fbc4a87afe9fe6af0fa67e81f672a9f9375d9585fba9310f81fc85', 0);
+(10, 'bobby', 'bob.bob@bob.bob', 'd8ba1ff038fbc4a87afe9fe6af0fa67e81f672a9f9375d9585fba9310f81fc85', 0),
+(11, 'amogus', 'noham.coulon@monemail.com', '04bb2936e3d4c28b89420f6f178b1e7854bc518e958272ad840eb52be817ce6e', 0);
 
 --
 -- Index pour les tables exportées
@@ -127,12 +129,12 @@ ALTER TABLE `home`
 -- AUTO_INCREMENT pour la table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
